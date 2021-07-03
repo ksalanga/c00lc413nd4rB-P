f33330dbccb407afc7523c4b5e4ebe38fd63f470
Kenneth Salanga <kennethsalanga@gmail.com>
2021-07-03 00:12:14 -0400

    form submission functionality almost completed. Date handling implementation not done

---
+++ components/Form.js
@@ -0,0 +1,37 @@
+function Form(props) {
+    function checkSubmission(e) {
+        e.preventDefault()
+        const submission = []
+        var privOrPublic = document.getElementsByName('privOrPublic')
+
+        privOrPublic.forEach((button) => {if (button.checked) submission.push(button.id)})
+        submission.push(document.getElementById("people").value, document.getElementById("startDay").value, document.getElementById("endDay").value)
+        var emptyEntry = false
+        submission.forEach((item) => {if (item === "") {
+            emptyEntry = true
+            return
+        }})
+        if (submission.length == 4 && !emptyEntry) {
+            props.submit(submission)
+            props.next()
+        }
+    } 
+    return(
+        <form>
+            <input type="radio" id="private" name="privOrPublic"/>Private
+            <input type="radio" id="public" name="privOrPublic"/>Public
+            <br/><br/>
+            <label htmlFor="people">Maximum Amount of People</label>
+            <input type="number" id="people"/>
+            <br/><br/>
+            <label htmlFor="startDay">Start Date</label>
+            <input type="date" id="startDay"/>
+            <label htmlFor="endDay">End Date</label>
+            <input type="date" id="endDay"/>
+            <br/><br/>
+            <input id="submit" type="submit" value="Submit" onClick={checkSubmission}/>
+        </form>
+    )
+}
+
+export default Form
