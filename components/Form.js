d83f9437153ced345e359f24f4ed3a6d8d5334cc
ksalanga <kennethsalanga@gmail.com>
2021-07-05 04:43:54 -0400

    added React Calendar for graphical range selection functionality and functionality to change the form values of the start date and end date given that selection. Also migrated buttons here so that the back function goes back to the form rather than start. prevented default on multiple buttons so functions don't run unintented

--- components/Form.js
+++ components/Form.js
@@ -1,16 +1,22 @@
-import {useEffect, useState} from 'react'
+import React, {useState} from 'react'
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css'
 
 function Form(props) {
     const [minDate, setMinDate] = useState('')
     const [maxDate, setMaxDate] = useState('')
+    const [graphicSelector, switchSelector] = useState(false)
 
-    function checkSubmission(e) {
-        e.preventDefault()
-        const submission = []
+    function checkSubmission() {
+        var people = document.getElementById("people")
+        var startDay = document.getElementById("startDay")
+        var endDay = document.getElementById("endDay")
+        if (people == null || startDay == null || endDay == null) return
+        var submission = []
         var privOrPublic = document.getElementsByName('privOrPublic')
 
         privOrPublic.forEach((button) => {if (button.checked) submission.push(button.id)})
-        submission.push(document.getElementById("people").value, document.getElementById("startDay").value, document.getElementById("endDay").value)
+        submission.push(people.value, startDay.value, endDay.value)
         var emptyEntry = false
         submission.forEach((item) => {if (item === "") {
             emptyEntry = true
@@ -35,25 +41,66 @@ function Form(props) {
     }
 
     return(
-        <form onSubmit={(e) => e.preventDefault()}>
+        graphicSelector ?
+        <>
+        <button onClick={(e) => {
+            e.preventDefault()
+            switchSelector(false)
+        }}>⬅️</button>
+        <Calendar onChange={(dates) => {
+            setMinDate(formatDate(dates[0]))
+            setMaxDate(formatDate(dates[1]))
+            switchSelector(false)
+        }} selectRange={true}
+        minDetail={"year"} minDate={new Date()}/>
+        </>
+        :
+        <>
+        <button onClick={(e) => {
+            e.preventDefault()
+            props.setStep(0)
+        }}>⬅️</button>
+        <form onSubmit={(e) => {
+            e.preventDefault()
+            checkSubmission()
+        }}>
             <input type="radio" id="private" name="privOrPublic"/>Private
             <input type="radio" id="public" name="privOrPublic"/>Public
             <br/><br/>
             <label htmlFor="people">Maximum Amount of People</label>
-            <input type="number" id="people" min="1"/>
+            <input type="number" id="people" min="1" onSubmit={(e) => {e.preventDefault()}}/>
             <br/><br/>
             <label htmlFor="startDay">Start Date</label>
-            <input type="date" id="startDay" name="chooseDate" min={new Date().toISOString().split('T')[0]} onChange={handleChange}/>
+            <input type="date" id="startDay" name="chooseDate" value={minDate} min={new Date().toISOString().split('T')[0]} onChange={handleChange}/>
             { minDate != "" &&
                 <>
-                    <label htmlFor="endDay">End Date</label>
-                    <input type="date" id="endDay" name="chooseDate" min={minDate} onChange={(e) => setMaxDate(e.target.value)}/>
+                    <label htmlFor="endDay" style={{marginLeft: "10px"}}>End Date</label>
+                    <input type="date" id="endDay" name="chooseDate" value={maxDate} min={minDate} onChange={(e) => setMaxDate(e.target.value)}/>
                     <br/><br/>
-                    <input id="submit" type="submit" value="Submit" onClick={checkSubmission}/>
+                    <button id="submit" type="submit" value="Submit">Submit</button>
                 </>
             }
         </form>
+        <button onClick={(e) => {
+            e.preventDefault()
+            switchSelector(true)
+        }} style={{marginLeft: "10px"}}>📅 graphical selector</button>
+        </>
     )
+}
+
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
 }
 
 export default Form
