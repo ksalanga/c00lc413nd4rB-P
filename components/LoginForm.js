0af227e8c2797a9fc70e2f80f32341cd63fabb16
ksalanga <kennethsalanga@gmail.com>
2021-07-29 01:28:37 -0400

    added barebones login Form no CSS, but with a functionality to hide password or show it

---
+++ components/LoginForm.js
@@ -0,0 +1,25 @@
+
+
+export default function LoginForm() {
+    return (
+        <>
+        <form onSubmit={() => {
+
+        }}>
+            Username <input type="text" />
+            <br></br>
+            <br></br>
+            Password <input type="password" />
+        </form>
+
+        <button onClick={() => {
+                var password = document.getElementsByTagName('input')[1]
+                if (password.getAttribute('type') === 'password') {
+                    password.setAttribute('type', 'text')
+                } else {
+                    password.setAttribute('type', 'password')
+                }
+        }}>toggle pass</button>
+        </>
+    )
+}
