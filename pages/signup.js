2d7d48d2d26d47dce931d87ca5d05c19af150942
ksalanga <kennethsalanga@gmail.com>
2021-07-30 22:58:42 -0400

    removed mongodb util and getServersideprops as it is not needed

--- pages/signup.js
+++ pages/signup.js
@@ -1,31 +1,10 @@
-import { connectToDatabase } from '../util/mongodb.js'
 import SignupForm from '../components/SignupForm.js'
 
-export default function Signup({ users }) { 
+export default function Signup() { 
     return (
         <>
-        <ul>
-        {users.map((user, i) => (
-          <li key={i}>
-            <h2>{user.name}</h2>
-            <h3>{user.email}</h3>
-          </li>
-        ))}
-      </ul>
         <h1>Hi</h1>
         <SignupForm />
         </>
     )
-}
-
-export async function getServerSideProps() {
-    const { db } = await connectToDatabase()
-
-    const users = await db.collection('users').find({}).toArray()
-
-    return {
-        props: {
-            users: JSON.parse(JSON.stringify(users))
-        },
-    }
 }
