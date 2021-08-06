818325e6857a1f6c5233572b91d5a5cf6450930e
ksalanga <kennethsalanga@gmail.com>
2021-08-06 04:42:47 -0400

    adding passport authenticate. none of the passport modules are running. must fix

--- pages/api/users/login.js
+++ pages/api/users/login.js
@@ -1,41 +1,32 @@
-// Express Session
-// Session Storage (Can implement Mongo Store later)
-// Passport for Sessions and Cookies
+
 
 import nextConnect from 'next-connect'
 import passport from 'passport'
-import { Strategy } from 'passport-local'
+import Local from 'passport-local'
 import { default as dbConnect } from '../../../middleware/database.js'
 import { compare } from 'bcrypt'
 
 
 const handler = nextConnect()
+
 handler.use(passport.initialize())
 handler.use(dbConnect)
 
-const LocalStrategy = Strategy
-
-
-handler.post((req, res) => {
+handler.post(async (req, res) => {
     const db = req.db
 
-    passport.authenticate('local',  {
-        successRedirect: '/',
-        failureRedirect: 'login',
-    })
-
-    passport.use(new LocalStrategy(
-        (username, password, done) => {
+    passport.use(new Local.Strategy(
+        function (username, password, done) {
             try {
-                console.log('run?')
-                const userFound = db.findOne({username: username})
-
+                console.log('local')
+                const userFound = db.collection('users').findOne({username: username})
+    
                 if (!userFound) return done(null, false, {message: 'No User'})
                 
                 const hash = userFound.password
-
+    
                 const passwordFound = compare(password, hash)
-
+    
                 if (!passwordFound) return done(null, false, {message: 'Incorrect password'})
                 return done (null, userFound)
             } catch (e) {
@@ -43,6 +34,17 @@ handler.post((req, res) => {
             }
         }
     ))
+
+    await passport.authenticate('local',  {
+        session: false
+    }, function (error, token) {
+        console.log('auth')
+        if (error) {
+            console.log(error)
+        } else {
+            console.log(token)
+        }
+    })
 
     res.status(200).json({message: 'successfully logged in'})
 })
