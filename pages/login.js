b5f58c670b13d436441575204e629d3b52bbaf46
ksalanga <kennethsalanga@gmail.com>
2021-08-07 01:22:37 -0400

    basic login page with Fetch working (although it isn't getting the full response. might have to use .then)

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,30 @@
 import Head from 'next/head'
 import 'bootstrap/dist/css/bootstrap.css'
+import { useState } from 'react'
 
-function Login({ Component, pageProps }) {
+function Login() {
+  const [username, setUserName] = useState('')
+  const [password, setPassword] = useState('')
+
+  const checkSubmission = async () => {
+    if (username === '' || password === '') return
+
+    const payload = {
+      username: username,
+      password: password
+    }
+
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    })
+
+    console.log(response)
+  }
+
   return (
     <>
       <Head>
@@ -9,6 +32,17 @@ function Login({ Component, pageProps }) {
         <title>Login</title>
       </Head>
       <h1>LOGIN</h1>
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        checkSubmission()
+      }}>
+        Username <input type='text' onChange={(e) => setUserName(e.target.value)}></input>
+        <br>
+        </br>
+        Password <input type='password' onChange={(e) => setPassword(e.target.value)}></input>
+        <br></br>
+        <button type='submit'>Submit</button>
+      </form>
     </>
   )
 }
