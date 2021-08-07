1d2f2ff8293e6ba482c45d36256adfd25428ed9f
Kenneth Salanga <kennethsalanga@gmail.com>
2021-08-06 21:53:25 -0400

    imported user model rather than Connect middleware, seems to have made Passport usage work

--- pages/api/users/login.js
+++ pages/api/users/login.js
@@ -1,51 +1,34 @@
-
-
 import nextConnect from 'next-connect'
 import passport from 'passport'
 import Local from 'passport-local'
-import { default as dbConnect } from '../../../middleware/database.js'
-import { compare } from 'bcrypt'
-
+import UserDataModel from '../../../models/UserDataModel'
 
 const handler = nextConnect()
+const Users = new UserDataModel()
 
 handler.use(passport.initialize())
-handler.use(dbConnect)
 
-handler.post(async (req, res) => {
-    const db = req.db
+passport.use(new Local.Strategy(
+    (username, password, done) => {
+        try {
+            const userFound = Users.findUser(username)
 
-    passport.use(new Local.Strategy(
-        function (username, password, done) {
-            try {
-                console.log('local')
-                const userFound = db.collection('users').findOne({username: username})
-    
-                if (!userFound) return done(null, false, {message: 'No User'})
-                
-                const hash = userFound.password
-    
-                const passwordFound = compare(password, hash)
-    
-                if (!passwordFound) return done(null, false, {message: 'Incorrect password'})
-                return done (null, userFound)
-            } catch (e) {
-                return done(e)
-            }
+            if (!userFound) return done(null, false, {message: 'No User'})
+
+            console.log(username)
+            
+            const passwordFound = Users.matchingPassword(username, password)
+
+            if (!passwordFound) return done(null, false, {message: 'Incorrect password'})
+            return done (null, userFound)
+        } catch (e) {
+            return done(e)
         }
-    ))
+    }
+))
 
-    await passport.authenticate('local',  {
-        session: false
-    }, function (error, token) {
-        console.log('auth')
-        if (error) {
-            console.log(error)
-        } else {
-            console.log(token)
-        }
-    })
-
+handler.post( passport.authenticate('local', { successRedirect: '/',
+failureRedirect: '/login' }), (req, res) => {
     res.status(200).json({message: 'successfully logged in'})
 })
 
