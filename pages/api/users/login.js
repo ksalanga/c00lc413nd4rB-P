e31d812ce56654a9d3fe7b1cd0b1c4b5996fd685
ksalanga <kennethsalanga@gmail.com>
2021-08-07 01:23:50 -0400

    added sessions for users. although the functionality is not clear. makes a cookie and creates a session but idk how to check this. Might also want to store sessions in Mongo cause right now it's storing in files

--- pages/api/users/login.js
+++ pages/api/users/login.js
@@ -2,32 +2,57 @@ import nextConnect from 'next-connect'
 import passport from 'passport'
 import Local from 'passport-local'
 import UserDataModel from '../../../models/UserDataModel'
+import session from 'express-session'
+import sessionFileStore from 'session-file-store'
+import flash from 'connect-flash'
 
+const sessionCookie = 'calendar.sid'
 const handler = nextConnect()
 const Users = new UserDataModel()
+const FileStore = sessionFileStore(session)
+
+handler.use(session({
+    store: new FileStore({ path: 'sessions' }),
+    secret: 'some random thingy',
+    resave: true,
+    saveUninitialized: true, 
+    name: sessionCookie
+}))
 
 handler.use(passport.initialize())
+handler.use(passport.session())
+handler.use(flash())
 
 passport.use(new Local.Strategy(
-    (username, password, done) => {
+    async (username, password, done) => {
         try {
-            const userFound = Users.findUser(username)
+            const userFound = await Users.findUser(username)
 
             if (!userFound) return done(null, false, {message: 'No User'})
 
-            const passwordFound = Users.matchingPassword(username, password)
+            const passwordFound = await Users.matchingPassword(username, password)
 
             if (!passwordFound) return done(null, false, {message: 'Incorrect password'})
-            return done (null, userFound)
+            return done(null, userFound)
         } catch (e) {
             return done(e)
         }
     }
 ))
 
-handler.post( passport.authenticate('local', { successRedirect: '/',
-failureRedirect: '/login' }), (req, res) => {
-    res.status(200).json({message: 'successfully logged in'})
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+  
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+handler.post(passport.authenticate('local', {
+    failureFlash: true 
+}), (req, res) => {
+    res.statusCode = 200
+    res.json({'message': 'logged in'})
 })
 
 export default handler
