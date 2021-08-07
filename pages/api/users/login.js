b31f5c3ed08b9dce9a8dfe0888ff8371468ad123
Kenneth Salanga <kennethsalanga@gmail.com>
2021-08-07 04:36:14 -0400

    cleaned up Login API immensely by moving middleware initalizations to a different middleware directory. Added some comments for the current plan for tomorrow (aka today)

--- pages/api/users/login.js
+++ pages/api/users/login.js
@@ -1,58 +1,21 @@
 import nextConnect from 'next-connect'
 import passport from 'passport'
-import Local from 'passport-local'
-import UserDataModel from '../../../models/UserDataModel'
-import session from 'express-session'
-import sessionFileStore from 'session-file-store'
-import flash from 'connect-flash'
+import { default as authenticate } from '../../../middleware/authenticate'
 
-const sessionCookie = 'calendar.sid'
 const handler = nextConnect()
-const Users = new UserDataModel()
-const FileStore = sessionFileStore(session)
+handler.use(authenticate)
 
-handler.use(session({
-    store: new FileStore({ path: 'sessions' }),
-    secret: 'some random thingy',
-    resave: true,
-    saveUninitialized: true, 
-    name: sessionCookie
-}))
-
-handler.use(passport.initialize())
-handler.use(passport.session())
-handler.use(flash())
-
-passport.use(new Local.Strategy(
-    async (username, password, done) => {
-        try {
-            const userFound = await Users.findUser(username)
-
-            if (!userFound) return done(null, false, {message: 'No User'})
-
-            const passwordFound = await Users.matchingPassword(username, password)
-
-            if (!passwordFound) return done(null, false, {message: 'Incorrect password'})
-            return done(null, userFound)
-        } catch (e) {
-            return done(e)
-        }
-    }
-))
-
-passport.serializeUser(function(user, done) {
-    done(null, user);
-});
-  
-passport.deserializeUser(function(user, done) {
-    done(null, user);
-});
-
+// Login Pop Up Message,
+// Either through failureFlash or statusCode 400 with a given message.
+// React Router? or set up a gateway server and/or reverse proxy that deals with what's handled
 handler.post(passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
     failureFlash: true 
-}), (req, res) => {
-    res.statusCode = 200
-    res.json({'message': 'logged in'})
-})
+}))
+// , (req, res) => {
+//     res.statusCode = 200
+//     res.json({'message': 'logged in'})
+// })
 
 export default handler
