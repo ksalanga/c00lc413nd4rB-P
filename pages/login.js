d779cae0f885f030663a6d3ac604f99df3d36ed5
Kenneth Salanga <kennethsalanga@gmail.com>
2021-08-06 21:52:30 -0400

    created new login page for API login direct

---
+++ pages/login.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import 'bootstrap/dist/css/bootstrap.css'
+
+function Login({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Login</title>
+      </Head>
+      <h1>LOGIN</h1>
+    </>
+  )
+}
+
+export default Login
