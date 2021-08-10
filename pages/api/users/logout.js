b0635f476b26e6bb2e781528f72b5928d46da501
Kenneth Salanga <kennethsalanga@gmail.com>
2021-08-10 05:59:40 -0400

    initializing logout. Session and logout functionality work, just need to figure out how to destroy our cookie. Either using setHeader or req.session.cookie. req.session.cookie seems to be different from the actual calendar.sid cookie tho

--- pages/api/users/logout.js
+++ pages/api/users/logout.js
@@ -1,2 +1,16 @@
-// Delete Session
-// Delete cookies
+import nextConnect from 'next-connect'
+import {default as authenticateMiddleware, sessionCookie} from '../../../middleware/authenticate'
+import cookie from 'cookie'
+
+const handler = nextConnect()
+handler.use(authenticateMiddleware)
+
+handler.get((req, res) => {
+    req.session.cookie.expires = "Thu, Jan 01 1970 00:00:00 UTC"
+    req.session.destroy()
+    req.logOut()
+    // res.setHeader('Set-Cookie', cookie.serialize(sessionCookie, req.cookies.sessionCookie, { expires: "Thu, Jan 01 1970 00:00:00 UTC"}))
+    res.status(200).json({'message' : 'logged out'})
+})
+
+export default handler
