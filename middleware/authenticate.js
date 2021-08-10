4b005bb5a95754ac021140afdede8e2dad469f9f
ksalanga <kennethsalanga@gmail.com>
2021-08-10 02:10:49 -0400

    migrated passport local functionality to a separate middleware

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,56 +1,13 @@
 import nextConnect from 'next-connect'
 import passport from 'passport'
-import Local from 'passport-local'
-import UserDataModel from '../models/UserDataModel'
-import session from 'express-session'
-import sessionFileStore from 'session-file-store'
 import next from 'next'
+import { default as localPassport } from './PassportLocal'
 
 const handler = nextConnect()
-export const sessionCookie = 'calendar.sid'
-const Users = new UserDataModel()
-const FileStore = sessionFileStore(session)
 
-handler.use(session({
-    store: new FileStore({ path: 'sessions' }),
-    secret: 'some random thingy',
-    resave: true,
-    saveUninitialized: true, 
-    name: sessionCookie
-}))
+passport.use(localPassport)
 
 handler.use(passport.initialize())
-handler.use(passport.session())
-
-passport.use(new Local.Strategy(
-    async (username, password, done) => {
-        try {
-            const userFound = await Users.findUser(username)
-
-            if (!userFound) return done(null, false, {message: 'No User'})
-
-            const passwordFound = await Users.matchingPassword(username, password)
-
-            if (!passwordFound) return done(null, false, {message: 'Incorrect password'})
-            return done(null, userFound)
-        } catch (e) {
-            return done(e)
-        }
-    }
-))
-
-passport.serializeUser((user, done) => {
-    try {
-        done(null, user.username)
-    } catch (e) { done(e) }
-})
-  
-passport.deserializeUser(async (username, done) => {
-    try {
-        var user = await Users.findUser(username)
-        done(null, user)
-    } catch (e) { done(e) }
-})
 
 export const passportAuthenticate = (req, res) => {
     passport.authenticate('local', function (err, user, info) {
