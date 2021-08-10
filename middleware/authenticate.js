fd72a9d75061b2daaa700f1e2ffa38e73f238920
Kenneth Salanga <kennethsalanga@gmail.com>
2021-08-10 05:57:35 -0400

    authenticate now only used to initialize express session and mongo store

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,20 +1,16 @@
 import nextConnect from 'next-connect'
-import passport from 'passport'
-import next from 'next'
-import { default as localPassport } from './PassportLocal'
+import session from 'express-session'
+import MongoStore from 'connect-mongo'
+export const sessionCookie = 'calendar.sid'
 
 const handler = nextConnect()
 
-passport.use(localPassport)
-
-handler.use(passport.initialize())
-
-export const passportAuthenticate = (req, res) => {
-    passport.authenticate('local', function (err, user, info) {
-        if (err) return res.status(401).json(err)
-        if (user) res.status(200).json({'message': 'logged in'})
-        else res.status(401).json({'message': info.message})
-    })(req, res, next)
-}
+handler.use(session({
+    store: new MongoStore({ mongoUrl: process.env.MONGODB_URI}),
+    secret: 'some random thingy',
+    resave: true,
+    saveUninitialized: true, 
+    name: sessionCookie
+}))
 
 export default handler
