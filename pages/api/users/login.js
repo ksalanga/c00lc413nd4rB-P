a27395d08430d0353e12ffced9ab0c1f6ad364e1
Kenneth Salanga <kennethsalanga@gmail.com>
2021-08-10 05:58:39 -0400

    finally got serialize user to work under the passport middleware. once authenticated with a user, request will login, essentially creating a new user Object in req.user for serialize to scan

--- pages/api/users/login.js
+++ pages/api/users/login.js
@@ -1,16 +1,19 @@
 import nextConnect from 'next-connect'
-import { default as authenticate, passportAuthenticate } from '../../../middleware/authenticate'
+import { default as authenticationMiddleware } from '../../../middleware/authenticate'
+import { default as passport, authenticate } from '../../../middleware/passport'
 
 const handler = nextConnect()
-handler.use(authenticate)
+handler.use(authenticationMiddleware)
+handler.use(passport)
 
 const auth = (req, res, next) => {
+    console.log(req.session.cookie)
     if (req.isAuthenticated()) next()
     else res.status(400).json({'message': 'Not Authenticated.'})
 }
 
 handler.post((req, res) => {
-    passportAuthenticate(req, res)
+    authenticate(req, res)
 })
 
 handler.get(auth, (req, res) => {
