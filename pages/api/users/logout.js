0da7209977a29bebea5f49db7c63a443f9e1f066
ksalanga <kennethsalanga@gmail.com>
2021-08-11 18:59:35 -0400

    on logout, the current session if a user is logged in will now get deleted from the Client side (originally just on the server with req.session.destroy() and logout functions). Accomplished this by setting the maxAge to 0 on the session cookie within the exact path that it resides. This deletes the cookie so the server changes the response to no longer have the session cookie header.

--- pages/api/users/logout.js
+++ pages/api/users/logout.js
@@ -1,15 +1,14 @@
 import nextConnect from 'next-connect'
 import {default as authenticateMiddleware, sessionCookie} from '../../../middleware/authenticate'
-import cookie from 'cookie'
 
 const handler = nextConnect()
 handler.use(authenticateMiddleware)
 
 handler.get((req, res) => {
-    req.session.cookie.expires = "Thu, Jan 01 1970 00:00:00 UTC"
     req.session.destroy()
     req.logOut()
-    // res.setHeader('Set-Cookie', cookie.serialize(sessionCookie, req.cookies.sessionCookie, { expires: "Thu, Jan 01 1970 00:00:00 UTC"}))
+    // Setting Header deletes cookie on the client side
+    res.setHeader('Set-Cookie', `${sessionCookie}=${req.cookies[sessionCookie]}; Path=/; HttpOnly; max-age=0;`)
     res.status(200).json({'message' : 'logged out'})
 })
 
