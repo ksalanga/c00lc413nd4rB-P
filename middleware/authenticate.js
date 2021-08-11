d54899480dbb6599241d1117be690e8af6cadfdc
ksalanga <kennethsalanga@gmail.com>
2021-08-11 18:53:57 -0400

    sessions now expire in one day. checkAuthentication method moved over to this middleware

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -2,6 +2,7 @@ import nextConnect from 'next-connect'
 import session from 'express-session'
 import MongoStore from 'connect-mongo'
 export const sessionCookie = 'calendar.sid'
+const dayExpiration = 8.6 * (10**7) // one day in miliseconds
 
 const handler = nextConnect()
 
@@ -9,8 +10,14 @@ handler.use(session({
     store: new MongoStore({ mongoUrl: process.env.MONGODB_URI}),
     secret: 'some random thingy',
     resave: true,
-    saveUninitialized: true, 
-    name: sessionCookie
+    saveUninitialized: true,
+    name: sessionCookie,
+    cookie: { maxAge: dayExpiration }
 }))
+
+export const checkAuthentication = (req, res, next) => {
+    if (req.isAuthenticated()) next()
+    else res.status(400).json({'message': 'Not Authenticated.'})
+}
 
 export default handler
