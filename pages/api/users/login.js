a389fde2bf085de41275b4cbf0947e7cfaa2e0fa
ksalanga <kennethsalanga@gmail.com>
2021-08-11 18:54:35 -0400

    removed GET request since it was only used for testing. authenticationCheck is now in authentication middleware

--- pages/api/users/login.js
+++ pages/api/users/login.js
@@ -6,18 +6,8 @@ const handler = nextConnect()
 handler.use(authenticationMiddleware)
 handler.use(passport)
 
-const auth = (req, res, next) => {
-    if (req.isAuthenticated()) next()
-    else res.status(400).json({'message': 'Not Authenticated.'})
-}
-
 handler.post((req, res) => {
     authenticate(req, res)
 })
 
-handler.get(auth, (req, res) => {
-    res.status(200).end('OK')
-})
-
-handler.post()
 export default handler
