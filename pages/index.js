9199ba3dc58381eaa5b6b4b7e2664d47a64a28a7
ksalanga <kennethsalanga@gmail.com>
2021-08-12 17:52:05 -0400

    removed getServerSide props. not necessary since we can just use SWR as a fetch react hook

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import Head from 'next/head'
 import MainComponent from '../components/MainComponent'
 import React from 'react'
-import authenticate from '../middleware/authenticate'
-import nextConnect from 'next-connect'
 
 export default function Home() {
   return (
@@ -13,15 +11,4 @@ export default function Home() {
       <MainComponent />
     </>
   )
-}
-
-export async function getServerSideProps(context) {
-  // get ServerSideProps context is not recognizing the authenticated from the server session.
-  // try to run express handler but that doesn't work either
-  const handler = nextConnect()
-  handler.use(authenticate)
-
-  return {
-    props: {}
-  }
 }
