f9ff93f39979d371b4d99bf3eab1a69e26ef4085
ksalanga <kennethsalanga@gmail.com>
2021-08-12 23:21:29 -0400

    utility function for detecting if a user is present, if one is present do something. currently it just sends out a span element but we'll change that to a profile picture. Also it would be nice to save user Settings. Do this via the passport session and put values into React Context is a possibility

--- utils/user.js
+++ utils/user.js
@@ -3,9 +3,8 @@ import useSWR from 'swr'
 const fetcher = (url) => fetch(url).then(res => res.json())
 
 export default function Profile() {
-    const { data, error } = useSWR('/api/users/user', fetcher)
+    const { data, error } = useSWR('/api/users/userAuth', fetcher)
   
     if (error) return null
-    if (!data) return <div>loading...</div>
-    return data.user ? <div>hello {data.user}!</div> : null
+    return data?.user ? <span><b>{data.user}</b></span> : null
 }
