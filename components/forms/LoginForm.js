082a642a45c7d7b3234066788171ed5044d0ad00
ksalanga <kennethsalanga@gmail.com>
2021-08-12 23:17:02 -0400

    migrated login functionality to separate loginform component. Login page now includes navbar

--- components/forms/LoginForm.js
+++ components/forms/LoginForm.js
@@ -1,23 +1,58 @@
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import styles from '../../styles/login.module.css'
+
 export default function LoginForm() {
-    return (
+    const router = useRouter()
+    const [username, setUserName] = useState('')
+    const [password, setPassword] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const checkSubmission = async () => {
+        if (username === '' || password === '') return
+
+        const payload = {
+        username: username,
+        password: password
+        }
+
+        const response = await fetch('/api/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+        })
+
+        if (response.ok) {
+        router.push('/')
+        } else {
+        const message = await response.json()
+        setErrorMessage(message.message)
+        }
+    }
+
+    return(
         <>
-        <form onSubmit={() => {
-
+        <h1 className={styles.center}>LOGIN</h1>
+        {errorMessage !== '' && <div>{errorMessage}</div>}
+        <form className={styles.center} onSubmit={(e) => {
+            e.preventDefault()
+            checkSubmission()
         }}>
-            Username <input type="text" />
+            <div>Username</div>
+            <input type='text' onChange={(e) => setUserName(e.target.value)}></input>
+            <br></br>
+            <div>Password</div>
+            <input type='password' onChange={(e) => setPassword(e.target.value)}></input>
             <br></br>
             <br></br>
-            Password <input type="password" />
+            <button type='submit'>Submit</button>
         </form>
-
-        <button onClick={() => {
-                var password = document.getElementsByTagName('input')[1]
-                if (password.getAttribute('type') === 'password') {
-                    password.setAttribute('type', 'text')
-                } else {
-                    password.setAttribute('type', 'password')
-                }
-        }}>toggle pass</button>
+        <br></br>
+        <div className={styles.center}>Don't Have an Account?</div>
+        <b><div className={styles.center}><Link href="/signup">Signup</Link></div></b>
         </>
     )
 }
--- pages/login.js
+++ pages/login.js
@@ -1,37 +1,9 @@
 import Head from 'next/head'
 import 'bootstrap/dist/css/bootstrap.css'
-import { useState } from 'react'
-import { useRouter } from 'next/router'
+import Navbar from '../components/nav/Navbar'
+import LoginForm from '../components/Forms/LoginForm'
 
 function Login() {
-  const router = useRouter()
-  const [username, setUserName] = useState('')
-  const [password, setPassword] = useState('')
-  const [errorMessage, setErrorMessage] = useState('')
-
-  const checkSubmission = async () => {
-    if (username === '' || password === '') return
-
-    const payload = {
-      username: username,
-      password: password
-    }
-
-    const response = await fetch('/api/users/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(payload)
-    })
-
-    if (response.ok) {
-      router.push('/')
-    } else {
-      const message = await response.json()
-      setErrorMessage(message.message)
-    }
-  }
 
   return (
     <>
@@ -39,19 +11,8 @@ function Login() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Login</title>
       </Head>
-      <h1>LOGIN</h1>
-      {errorMessage !== '' && <div>{errorMessage}</div>}
-      <form onSubmit={(e) => {
-        e.preventDefault()
-        checkSubmission()
-      }}>
-        Username <input type='text' onChange={(e) => setUserName(e.target.value)}></input>
-        <br>
-        </br>
-        Password <input type='password' onChange={(e) => setPassword(e.target.value)}></input>
-        <br></br>
-        <button type='submit'>Submit</button>
-      </form>
+      <Navbar login={true}/>
+      <LoginForm />
     </>
   )
 }
