146669fc0f0012dd357b1947d088bba733965724
ksalanga <kennethsalanga@gmail.com>
2021-08-14 04:24:21 -0400

    logoutUser method added along with sending isLoading and isError on loggedInUser

--- utils/user.js
+++ utils/user.js
@@ -4,7 +4,13 @@ const fetcher = (url) => fetch(url).then(res => res.json())
 
 export default function loggedInUser() {
     const { data, error } = useSWR('/api/users/userAuth', fetcher)
-  
-    if (error) return null
-    return data?.user
+    return {
+        user: data,
+        isLoading: !error && !data,
+        isError: error
+    }
+}
+
+export async function logOutUser() {
+    return await fetch('/api/users/logout', {method: 'GET'})
 }
