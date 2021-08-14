0f3b6ca6e135459961f130a4272365dd3665c9e4
ksalanga <kennethsalanga@gmail.com>
2021-08-14 04:35:34 -0400

    responds with an end string rather than JSON so that the user Utility when calling the login API doesn't send an undefined or wrong Object error via the mongConnect and express sessions

--- pages/api/users/userAuth.js
+++ pages/api/users/userAuth.js
@@ -6,7 +6,8 @@ const handler = nextConnect()
 
 handler.use(auth)
 .get(checkAuthentication, (req, res) => {
-    res.status(200).json({ 'user': req.session.passport.user })
+    // Serialize only sends the user String, no other attributes
+    res.status(200).end(JSON.stringify({user: req.session.passport.user}))
 })
 
 export default handler
