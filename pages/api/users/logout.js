3b55a0da25dba91b9be6d09950ffcde14988339c
ksalanga <kennethsalanga@gmail.com>
2021-08-14 04:34:07 -0400

    logOut requests and responses could be stale so I added the extra measure to make the server wait for all of the request data to come in. Also, if a response gives an end rather than a json, this makes the logout functionality on the client side work perfectly as the response is now responsive and faster. So whenever the GET logout api is called, the response is almost guaranteed and doesn't cause issues with our LoginPage not receiving the correct response to go and reroute the page and rerender. This is because end simply sends a string while Json sends an object between the client and the server

--- pages/api/users/logout.js
+++ pages/api/users/logout.js
@@ -4,12 +4,12 @@ import {default as authenticateMiddleware, sessionCookie} from '../../../middlew
 const handler = nextConnect()
 handler.use(authenticateMiddleware)
 
-handler.get((req, res) => {
-    req.session.destroy()
-    req.logOut()
+handler.get(async (req, res) => {
+    await req.session.destroy()
+    await req.logOut()
     // Setting Header deletes cookie on the client side
     res.setHeader('Set-Cookie', `${sessionCookie}=${req.cookies[sessionCookie]}; Path=/; HttpOnly; max-age=0;`)
-    res.status(200).json({'message' : 'logged out'})
+    res.status(200).end(JSON.stringify({'message' : 'logged out'}))
 })
 
 export default handler
