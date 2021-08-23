889da60e0ed8298b0b243c7e0dab78506d7753fa
ksalanga <kennethsalanga@gmail.com>
2021-08-23 03:18:02 -0400

    delete API created which has many conditionals based on if the request doesn't have user or password, isn't applciation/json and the password from the user isn't correct to confirm the deletion. if all is alright, user will be deleted. if deleteCount is zero, user doesn't exist anymore

---
+++ pages/api/users/delete.js
@@ -0,0 +1,25 @@
+import nextConnect from "next-connect"
+const handler = nextConnect()
+import UserDataModel from '../../../models/UserDataModel'
+
+const UDM = new UserDataModel()
+
+handler.post(async (req, res) => {
+    const contentType = req.headers['content-type']
+
+    if (contentType === undefined || contentType !== 'application/json') return res.status(400).send('Content Type must be application/json')
+    const user = req.body['user']
+    const password = req.body['password']
+
+    if (user === undefined) return res.status(406).send('user field undefined')
+    if (password === undefined) return res.status(406).send('password field undefined')
+    if (Object.keys(req.body).length !== 2) return res.status(406).send('too little or too many properties. one password property only')
+
+    if (!(await UDM.matchingPassword(user, password))) return res.status(406).send('Incorrect Password')
+
+    if ((await UDM.deleteUser(user)).deletedCount) return res.status(200).send('User deleted')
+    
+    return res.status(404).send('user no longer exists')
+})
+
+export default handler
