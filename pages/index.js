d39c25384e456dc5c6747c460145649ac85bca0e
ksalanga <kennethsalanga@gmail.com>
2021-09-04 04:36:08 -0400

    loading Google Maps API script for place autocomplete, it's inputting twice into the header tho. fix that later

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,29 @@ import Navbar from '../components/nav/Navbar'
 import nextConnect from 'next-connect'
 import authenticate from '../middleware/authenticate'
 
+function loadScript(src, position, id) {
+  if (!position) {
+    return
+  }
+  
+  const script = document.createElement('script')
+  script.setAttribute('async', '')
+  script.setAttribute('id', id)
+  script.src = src
+  position.appendChild(script)
+}
+
 export default function Home({ user }) {
+  if (typeof window !== 'undefined') {
+    if (!document.querySelector('googleMaps')) {
+        loadScript(
+          'https://maps.googleapis.com/maps/api/js?key=AIzaSyBZ5KkgolEdqXA-8_fErDEbCY-fgDIeA-M&libraries=places',
+          document.querySelector('head'),
+          'googleMaps',
+        )
+    }
+  
+  }
   return (
     <>
       <Head>
