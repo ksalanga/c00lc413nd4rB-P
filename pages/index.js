5cc5af1190e066e234f78d4451da9be20016a6f6
ksalanga <kennethsalanga@gmail.com>
2021-09-17 04:06:34 -0400

    defer async + checking for process.browser stops next.js header from adding an async script twice due to rehydration. Also removed the convoluted loadScript tag and instead just added the script line in the next/head

--- pages/index.js
+++ pages/index.js
@@ -5,33 +5,12 @@ import Navbar from '../components/nav/Navbar'
 import nextConnect from 'next-connect'
 import authenticate from '../middleware/authenticate'
 
-function loadScript(src, position, id) {
-  if (!position) {
-    return
-  }
-  
-  const script = document.createElement('script')
-  script.setAttribute('async', '')
-  script.setAttribute('id', id)
-  script.src = src
-  position.appendChild(script)
-}
-
 export default function Home({ user }) {
-  if (typeof window !== 'undefined') {
-    if (!document.querySelector('googleMaps')) {
-        loadScript(
-          'https://maps.googleapis.com/maps/api/js?key=AIzaSyBZ5KkgolEdqXA-8_fErDEbCY-fgDIeA-M&libraries=places',
-          document.querySelector('head'),
-          'googleMaps',
-        )
-    }
-  
-  }
   return (
     <>
       <Head>
         <title>Calendar</title>
+        {process.browser && <script id='googleMaps' defer async src='https://maps.googleapis.com/maps/api/js?key=AIzaSyBZ5KkgolEdqXA-8_fErDEbCY-fgDIeA-M&libraries=places' />}
       </Head>
       <Navbar user={ user }/>
       <MainComponent />
