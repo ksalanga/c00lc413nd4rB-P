92589ee091d2de32724df8108759d1215f0c2b2b
ksalanga <kennethsalanga@gmail.com>
2021-09-20 04:39:19 -0400

    fetcher will first call timeZone API, to retrieve those values and add to the address object key in the final submission. Also, creator, attendingUsers, and decided keys are added to this final round of submission All of these values should be added on top of the original forms with the maxNum, # people, etc.. which then gets posted to the calendars create API. should work in theory. Haven't tested it yet. originally used setForm, but React doesn't allow updating state while also rendering a function.

--- components/utils/FormSubmission.js
+++ components/utils/FormSubmission.js
@@ -1,17 +1,43 @@
 import useSWR from "swr"
-const fetcher = async (...args) => fetch(...args).then(r => r.json())
+const fetcher = async (user, form, decided, ...args) => {
+    fetch(...args) // first fetches timeZone API
+    .then(r => r.json())
+    .then(data => {
+        const decidedOrUndecided = decided ? {decided: null} : {undecided: {confirmed: false}}
+        const submission = {...form,
+            creator: user.id, 
+            attendingUsers: [], 
+            decided: decidedOrUndecided,
+            address: {...form.address, 
+                timeZoneName: data.timeZoneName,
+                timeZoneId: data.timeZoneId,
+                rawOffset: data.rawOffset,
+                dstOffset: data.dstOffset,
+            }
+        }
+        return fetch('/api/calendars/create', { // then we make a post to calendars create
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(submission)
+        })
+    })
+    .then(r => r.json())
+}
 
-export default function FormSubmission(form) {
-
+export default async function FormSubmission(user, form, decided) {
     // We only get the timeZone of the minimum first date.
     // Calculate timestamp by subtracting minDate from new Date(0).
+    const lat = form.address.geoCode?.lat
+    const lng = form.address.geoCode?.lng
     const timeStamp = (
         (new Date(formatDate(form.dates[0])).getTime() - new Date(0).getTime()) * .001
     ).toString()
-    
-    const { data, error } = 
-    useSWR(`https://maps.googleapis.com/maps/api/timezone/json?location=${(form.latLng.lat).toString()}%2C${(form.latLng.lng).toString()}&timestamp=${timeStamp}&key=AIzaSyBZ5KkgolEdqXA-8_fErDEbCY-fgDIeA-M`, fetcher)
 
+    const { data, error } = 
+    useSWR(`https://maps.googleapis.com/maps/api/timezone/json?location=${(lat).toString()}%2C${(lng).toString()}&timestamp=${timeStamp}&key=AIzaSyBZ5KkgolEdqXA-8_fErDEbCY-fgDIeA-M`, fetcher)
+    
     return {
         timeZone: data,
         isLoading: !error && !data,
