b9d4b6802e82e7223a14227b0eb1fa2312664820
ksalanga <kennethsalanga@gmail.com>
2021-09-21 05:29:22 -0400

    added more errorHandling for creating Calendar. This push handles the dates array and selectionExpiration. Line 89-95 checks: if the form is decided, the form's selectionExpirationDate value must be null or else it will throw an error. Else, it's undecided. if the selectionExpirationDate isn't a Date type, then we throw an error.Lastly, if it's an Invalid date type, we throw an error. Next handlers deal with the dates array. First, we filter out any date that is less than today into the datesGreaterThanToday variable. (standardize later just lay out the functionality for now.) Next, if the filter returns length 0 we throw an error. If the datesGreaterThanToday Array is undecided and its length is less than 2, we throw an error. Lastly, we sort that entire array starting with the earliest day. If by now, it's undecided, we must check if expirationDate is before the earliest Date in the dates array and after today. If it's outside of those bounds, we throw an error.

--- pages/api/calendars/create.js
+++ pages/api/calendars/create.js
@@ -48,16 +48,17 @@ async function errorHandling(req, res) {
     || typeof(form['privateOrPublic']) !== 'string'
     || typeof(form['maximumPeople']) !== 'string'
     || (!(form['selectionExpirationDate'] instanceof Date)
-        && Object.prototype.toString.call(form['selectionExpirationDate']) !== '[object Null]')
+        && form['selectionExpirationDate'] !== null)
     || Array.isArray(form['dates'])
     || Array.isArray(form['attendingUsers'])
     || typeof(form['address']) !== 'object') {
         throw invalidFormatMessage
     }
 
+    const decidedOrUndecided = form['decidedOrUndecided']
     // Format for decided must be { decided: null } or { undecided : { confirmed : false }}
-    if (form['decidedOrUndecided']['undecided'] === undefined 
-    && form['decidedOrUndecided']['undecided'] === undefined) {
+    if (decidedOrUndecided['undecided'] === undefined 
+    && decidedOrUndecided['undecided'] === undefined) {
         throw `decidedOrUndecided Key in a create Calendar Submission must be an Object that has a decided key or undecided key.
         If the key to the decidedOrUndecided key is decided, it must have value null ( as it requires no further information ),
         If the key to the decidedOrUndecided key is undecided, it must have a object { confirmed: false } as a value.
@@ -65,8 +66,8 @@ async function errorHandling(req, res) {
         that confirmed key will be equal to true. )
         Example: { ...form, decidedOrUndecided: { decided: null } }
         OR { ...form, decidedOrUndecided: { undecided: { confirmed: false } } }`
-    } else if (form['decidedOrUndecided']['decided']) {// then the creator of the calendar has used decided
-        if (form['decidedOrUndecided']['decided'] !== null) {
+    } else if (decidedOrUndecided['decided']) {// then the creator of the calendar has used decided
+        if (decidedOrUndecided['decided'] !== null) {
             throw `If the Create Calendar Submission decidedOrUndecided value is of key decided, that key's value must be null:
             Ex: { ...form, decided: {decided: null} }`
         }
@@ -76,32 +77,60 @@ async function errorHandling(req, res) {
         { confirmed: false }
         Ex: { ...form, decidedOrUndecided: { undecided: { confirmed: false } } }`
 
-        if (form['decidedOrUndecided']['undecided']['confirmed'] === undefined) {
+        if (decidedOrUndecided['undecided']['confirmed'] === undefined) {
             throw incorrectUndecidedFormatMessage
         } else {
-            if (form['decidedOrUndecided']['undecided']['confirmed'] !== false) {
+            if (decidedOrUndecided['undecided']['confirmed'] !== false) {
                 throw incorrectUndecidedFormatMessage
             }
         }
     }
     
-    // Format for ExpirationDate. If isNaN(form['selectionExpirationDate'].getTime()) means not a valid date.
-    if (isNaN(form['selectionExpirationDate'].getTime())) {
-        throw `Invalid Date Format for key selectionExpiration Date`
+    if (decidedOrUndecided['decided'] && form['selectionExpirationDate'] !== null) {
+        throw 'If the calendar is decided, the form\'s selectionExpirationDate value must be null'
+    } else if (decidedOrUndecided['undecided'] && !(form['selectionExpirationDate'] instanceof Date)) {
+        throw 'If the calendar is undecided, the form\'s selectionExpirationDate value must be a Date type'
+    } else if (isNaN(form['selectionExpirationDate'].getTime())) {
+        throw 'If the calendar is undecided, the form\'s selectionExpirationDate value must be a valid Date'
     }
 
     // Format for Array of Dates: If every instance of that Object isn't a date, invalid.
+    // If Date is Invalid, invalid
     for (var date in form['dates']) {
         if (!(date instanceof Date)) {
             throw `Dates array must have Date Objects only`
         } else if (isNaN(date.getTime())){
-            throw 'Invalid Date Format for a date in the Date Array'
+            throw 'Invalid Date Format for a date in the dates Array'
         }
     }
-    // Filter Through Dates and Return anything that is greater than or equal to today.
-        // If it's undecided, must have length at least 2
-        // Sort Dates, if there are dates before today, invalid
 
+    // Mental Note for later: think about how changes in timeZone across the dates array will affect our filtering and error handling.
+
+    // Array of Date Values that are greater than or equal to today.
+    // just compare Month, Date, and Year rather than time.
+    // Must Standardize Today though to the actual Today of the Local TimeZone. Might be the 10th in Server's Date but the 9th in the User's Date.
+    // So a date in the 9th, which they added as a date, should have been accounted for.
+    // We've to ask the TimeZone.
+    const datesGreaterThanToday = form['dates'].filter(date => date.getTime() >= new Date().getTime())
+    // Revise THIS METHOD ^^^^, but filter in general.
+
+    if (datesGreaterThanToday.length === 0) {
+        throw 'Must have at least One Date in the Dates Array (Two if it\'s an undecided Calendar)'
+    }
+
+    if (datesGreaterThanToday.length < 2 && decidedOrUndecided['undecided']) {
+        throw 'An Undecided Calendar must have at least valid 2 dates in the dates array'
+    }
+    datesGreaterThanToday.sort((a, b)=>{return a.getTime() - b.getTime()})
+
+    // Anything with new Date() aka. Today requires tweaking because the time isn't 0'ed in hours, mins, seconds, and it isn't 12 am of the user's local comp.
+    // checks if Undecided first because if it's decided, then selectionExpirationDate will be null, if undecided is false, skips that && logic.
+    if (decidedOrUndecided['undecided'] 
+    && (form['selectionExpirationDate'].getTime() > datesGreaterThanToday[0].getTime()
+    || form['selectionExpirationDate'].getTime() < new Date().getTime())) {
+        throw 'selectionExpirationDate must be less than the Minimum selection Date and greater than Today'
+    }
+    
     // Format attendingUsers length must be 0
     if (form['attendingUsers'].length !== 0) {
         throw 'attendingUsers must be an empty array to start'
@@ -115,7 +144,7 @@ async function errorHandling(req, res) {
         // must have:
         // name (string)
 
-        // The rest we can call GoogleApis:
+        // The rest we can call GoogleApis and addonto the address object later:
         // geoCode (Object)
             // lat (Float)
             // lng (Float)
