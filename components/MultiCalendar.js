e1277f42a3d57ac60a7ac59d1f71ec862cc33c93
Kenneth Salanga <kennethsalanga@gmail.com>
2021-10-16 04:55:24 -0400

    just formatted the destructuring to add some space :^)

--- components/MultiCalendar.js
+++ components/MultiCalendar.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css'
 import animationStyles from '../styles/animations.module.css'
